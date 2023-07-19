c447ed3a774e194dd8606b6e547e13c679384c91
Aman Pawar <pawaraman00021@gmail.com>
2023-07-19 20:57:41 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,13 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
+  // let date = new Date();
+  // let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3></h3>
+          {/* <h3></h3> */}
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Aman Pawar</h3>
